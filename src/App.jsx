08f9ca6854f3dfc3b299fa01570477758589416a
pancysingla <102866027+pancysingla@users.noreply.github.com>
2025-03-22 06:04:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import Landing from './components/Landing'
+import { useState } from "react";
+import Landing from "./components/Landing";
 
 function App() {
-  return <Landing />
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    document.body.classList.toggle("dark-mode");
+    setDarkMode(!darkMode);
+  };
+
+  return <Landing darkMode={darkMode} toggleDarkMode={toggleDarkMode} />;
 }
 
-export default App
+export default App;
